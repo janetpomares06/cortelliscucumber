76051a3bee84b4a72ca6c408fde50eacbe35f693
Janet Pomares <pomares@strands.com>
2022-11-27 16:40:46 +0100

    Cleaning code

--- src/test/java/cortelliscucumber/CortellisCMCIntelligenceTests.java
+++ src/test/java/cortelliscucumber/CortellisCMCIntelligenceTests.java
@@ -18,29 +18,35 @@ public class CortellisCMCIntelligenceTests {
     private CortellisHomePage cortellisHomePage;
     private CustomerLoginsPage customerLoginsPage;
     private CortellisLoginPage cortellisLoginPage;
-    public static final String CORTELLIS_LOGIN_URL = "https://access.cortellis.com/login?app=cmc";
+    private String url;
+    public static final String URL = "https://access.cortellis.com/login?app=cmc";
+    public static final String GOOGLE_URL = "https://www.google.com/";
 
     public ChromeDriver driver = new ChromeDriver();
+
     @Given("A URL is provided")
     public void a_url_is_provided() {
-        driver.get("https://www.google.com/");
 
-        googlepage = new GooglePage(driver);
-        googlepage.acceptCookies();
+        url = URL;
         log.info("A URL is provided");
     }
+
     @When("Found Clarivate in a Google search and navigate through Clarivate pages to Cortellis home page")
     public void found_clarivate_in_a_google_search_and_navigate_through_clarivate_pages_to_cortellis_home_page() {
+        driver.get(GOOGLE_URL);
+        googlepage = new GooglePage(driver);
+        googlepage.acceptCookies();
         googlepage.enterKeyword();
         googlepage.goSearching();
         log.info("Found Clarivate in a Google search and navigate through Clarivate pages to Cortellis home page");
     }
+
     @Then("Access to Cortellis login page and check the URL")
     public void access_to_cortellis_login_page_and_check_the_url() {
         googlesearchpage = new GoogleSearchPage(driver);
         clarivateHomePage = new ClarivateHomePage(driver);
-        genericsManufacturingPage =  new GenericsManufacturingPage(driver);
-        cortellisHomePage  = new CortellisHomePage(driver);
+        genericsManufacturingPage = new GenericsManufacturingPage(driver);
+        cortellisHomePage = new CortellisHomePage(driver);
         customerLoginsPage = new CustomerLoginsPage(driver);
         cortellisLoginPage = new CortellisLoginPage(driver);
         googlesearchpage.enterClarivate();
@@ -50,10 +56,7 @@ public class CortellisCMCIntelligenceTests {
         genericsManufacturingPage.goCortellisCMCIntelligence();
         cortellisHomePage.loginCortellis();
         customerLoginsPage.loginCortellis();
-        cortellisLoginPage.getUrl();
-        Assertions.assertEquals(cortellisLoginPage.getUrl(),CORTELLIS_LOGIN_URL);
-        log.info("Access to Cortellis login page and check the URL");
-
+        Assertions.assertEquals(url, cortellisLoginPage.getUrl(), "The URL provided is not the URL reached through the steps");
     }
 
 
--- src/test/java/cortelliscucumber/RunCucumberTest.java
+++ src/test/java/cortelliscucumber/RunCucumberTest.java
@@ -1,6 +1,5 @@
 package cortelliscucumber;
 
-import cucumber.api.CucumberOptions;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -12,6 +11,5 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("cortelliscucumber")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-//@CucumberOptions(tags = {"@Step1, @Step2, @Step3, @Step4"})
 public class RunCucumberTest {
 }
